4844550feaa408c7ddbf0317a5c2dd1f812d0d65
Kjetil Svalestuen <kjetil.svalestuen@bekk.no>
2018-09-10 09:05:16 +0200

    Bytt side programmatisk for å forenkle CSS

--- src/app/connected-components/frontpage/Foreldrepenger.tsx
+++ src/app/connected-components/frontpage/Foreldrepenger.tsx
@@ -1,14 +1,24 @@
-import * as React from 'react';
+import React, { StatelessComponent } from 'react';
 import KnappBase from 'nav-frontend-knapper';
 import TypografiBase from 'nav-frontend-typografi';
 import Lenke from 'nav-frontend-lenker';
-import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 import UserHelp from '../../components/frontpage/user-help/UserHelp';
 import NavigationBox from '../../components/frontpage/navigation-box/NavigationBox';
 import translate from '../../utils/translate';
 
-const Foreldrepenger = ({ cls }: { cls: any }) => {
+interface ForeldrepengerProps {
+    cls: any;
+    history: any;
+    location: any;
+    match: any;
+}
+
+const Foreldrepenger: StatelessComponent<ForeldrepengerProps> = ({
+    cls,
+    history
+}) => {
     return (
         <NavigationBox title={translate('foreldrepenger')}>
             <TypografiBase type="normaltekst">
@@ -20,13 +30,14 @@ const Foreldrepenger = ({ cls }: { cls: any }) => {
             <div className={cls.element('filler')} />
             <div className={cls.element('navigation-section')}>
                 <div className={cls.element('double-buttons')}>
-                    <Link to="/hva-vil-du-soke-om/foreldrepenger">
-                        <KnappBase
-                            className={cls.element('knapp')}
-                            type="hoved">
-                            {translate('søk_foreldrepenger')}
-                        </KnappBase>
-                    </Link>
+                    <KnappBase
+                        className={cls.element('knapp')}
+                        type="hoved"
+                        onClick={() => {
+                            history.push('/hva-vil-du-soke-om/foreldrepenger');
+                        }}>
+                        {translate('søk_foreldrepenger')}
+                    </KnappBase>
                     <KnappBase className={cls.element('knapp')} type="standard">
                         {translate('har_søkt_foreldrepenger')}
                     </KnappBase>
@@ -42,4 +53,4 @@ const Foreldrepenger = ({ cls }: { cls: any }) => {
     );
 };
 
-export default Foreldrepenger;
+export default withRouter(Foreldrepenger);
