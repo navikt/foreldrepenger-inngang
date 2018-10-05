6fbc027113518e5e53d0c083d962264c0e01658f
Kjetil Svalestuen <kjetil.svalestuen@bekk.no>
2018-10-05 14:41:28 +0200

    Legg til illustrasjon under ferie-eksempel

--- src/app/pages/om-foreldrepenger/ferie/Ferie.tsx
+++ src/app/pages/om-foreldrepenger/ferie/Ferie.tsx
@@ -6,6 +6,7 @@ import LesMer from '../../../components/les-mer/LesMer';
 import TypografiBase from 'nav-frontend-typografi';
 import BEMHelper from '../../../utils/bem';
 import { getContent } from '../../../utils/getContent';
+import Ferieforskyvning from './Ferieforskyvning';
 import './ferie.less';
 
 const cls = BEMHelper('ferie');
@@ -24,28 +25,21 @@ const Ferie: React.StatelessComponent<Props & IntlProps> = ({ id, lang }) => {
     return (
         <PanelMedIllustrasjon id={id} svg={pageSvg} title={getTranslation('ferie', lang)}>
             <StrukturertTekst tekst={getContent(content, lang)} />
-
-            <FerieEksempel title={getTranslation('om_foreldrepenger.ferie.eksempel_label', lang)} />
-            <FerieEksempel
-                title={getTranslation('om_foreldrepenger.ferie.eksempel2_label', lang)}
-            />
-
+            <div className={cls.element('eksempel')}>
+                <TypografiBase type="element">
+                    {getTranslation('om_foreldrepenger.ferie.eksempel_label')}
+                </TypografiBase>
+                <Ferieforskyvning />
+            </div>
             <LesMer intro={getTranslation('om_foreldrepenger.ferie.rett_til_utsettelse', lang)}>
                 <StrukturertTekst tekst={getContent(rettTilUtsettelseContent, lang)} />
             </LesMer>
             <LesMer intro={getTranslation('om_foreldrepenger.ferie.utsette', lang)}>
                 <StrukturertTekst tekst={getContent(fåUtsettelseContent, lang)} />
             </LesMer>
-
             <StrukturertTekst tekst={getContent(feriepenger, lang)} />
         </PanelMedIllustrasjon>
     );
 };
-
-const FerieEksempel = ({ title }: { title: string }) => (
-    <div className={cls.element('eksempel')}>
-        <TypografiBase type="element">{title}</TypografiBase>
-    </div>
-);
 
 export default withIntl(Ferie);
---
+++ src/app/pages/om-foreldrepenger/ferie/Ferieforskyvning.tsx
@@ -0,0 +1,185 @@
+import * as React from 'react';
+
+const Ferieforskyvning = () => (
+    <svg height="100px" width="100%" version="1.1" viewBox="0 0 622 100">
+        <defs />
+        <g id="Illustrasjon---Ferie-forskyvning-Copy" fill="none" stroke="none" strokeWidth="1">
+            <g id="Group-7" transform="translate(1.000000, 19.000000)">
+                <g id="Group-5" transform="translate(0.000000, 22.000000)">
+                    <rect
+                        height="7"
+                        id="Rectangle-2"
+                        width="247"
+                        fill="#E3ECF5"
+                        rx="3.5"
+                        stroke="#979797"
+                        strokeWidth="0.5"
+                        x="33"
+                        y="25"
+                    />
+                    <rect
+                        height="7"
+                        id="Rectangle-2"
+                        width="207"
+                        fill="#E3ECF5"
+                        rx="3.5"
+                        stroke="#979797"
+                        strokeWidth="0.5"
+                        x="390"
+                        y="25"
+                    />
+                    <rect
+                        height="7"
+                        id="Rectangle-2"
+                        width="10"
+                        fill="#7F756C"
+                        rx="3.5"
+                        stroke="#746A60"
+                        strokeWidth="0.5"
+                        x="481"
+                        y="25"
+                    />
+                    <rect
+                        height="7"
+                        id="Rectangle-2"
+                        width="10"
+                        fill="#3E3832"
+                        rx="3.5"
+                        stroke="#3E3832"
+                        strokeWidth="0.5"
+                        x="587"
+                        y="25"
+                    />
+                    <rect
+                        height="7"
+                        id="Rectangle-2"
+                        width="30"
+                        fill="#E3ECF5"
+                        rx="3.5"
+                        stroke="#979797"
+                        strokeWidth="0.5"
+                        x="0"
+                        y="25"
+                    />
+                    <rect
+                        height="7"
+                        id="Rectangle-2"
+                        width="100"
+                        fill="#159345"
+                        rx="3.5"
+                        x="285"
+                        y="25"
+                    />
+                    <g id="2-uker-ferie" transform="translate(300, 12)">
+                        <g
+                            id="Group-2"
+                            stroke="#159345"
+                            strokeLinecap="round"
+                            strokeWidth="0.75"
+                            transform="translate(25, 30)">
+                            <path
+                                id="Stroke-8244"
+                                d="M16.4516129,11.516129 L12.0608572,11.516129 C10.2672334,11.516129 8.67924346,12.1643226 7.67741935,13.1612903"
+                            />
+                            <path
+                                id="Stroke-8245"
+                                d="M12.0645161,11.516129 L9.87096774,5.48387097"
+                            />
+                            <path id="Stroke-8246" d="M7.87977419,0.509451613 L7.67741935,0" />
+                            <path
+                                id="Stroke-8247"
+                                d="M7.73019148,0.841387636 C4.23605814,2.26751228 1.9675218,4.88883709 2.9875394,7.53967302 L3.25113946,8.22580645 L4.91583111,7.54557524 L5.31839424,6.58793955 L6.24815748,7.00257072 L12.9069241,4.28385923 L13.3087709,3.32696132 L14.2385342,3.74011693 L15.9032258,3.0606235 L15.6396258,2.37449007 C14.6188918,-0.277083633 11.2236085,-0.584737009 7.73019148,0.841387636 L7.73019148,0.841387636 Z"
+                            />
+                            <path
+                                id="Stroke-8248"
+                                d="M17,16.4516129 C16.2608696,16.4516129 15.5217391,15.7145806 15.5217391,14.8064516 C15.5217391,15.7145806 14.7826087,16.4516129 14.0434783,16.4516129 C13.3043478,16.4516129 12.5652174,15.7145806 12.5652174,14.8064516 C12.5652174,15.7145806 11.826087,16.4516129 11.0869565,16.4516129 C10.3478261,16.4516129 9.60869565,15.7145806 9.60869565,14.8064516 C9.60869565,15.7145806 8.86956522,16.4516129 8.13043478,16.4516129 C7.39130435,16.4516129 6.65217391,15.7145806 6.65217391,14.8064516 C6.65217391,15.7145806 5.91304348,16.4516129 5.17391304,16.4516129 C4.43478261,16.4516129 3.69565217,15.7145806 3.69565217,14.8064516 C3.69565217,15.7145806 2.95652174,16.4516129 2.2173913,16.4516129 C1.47826087,16.4516129 0.739130435,15.7145806 0.739130435,14.8064516 C0.739130435,15.3740323 0.396913043,15.8749839 0,16.1711129"
+                            />
+                        </g>
+                        <text
+                            id="2-uker-ferie"
+                            fill="#3E3832"
+                            fontFamily="SourceSansPro-Bold, Source Sans Pro"
+                            fontSize="14"
+                            fontWeight="bold">
+                            <tspan x="0" y="0">
+                                2 uker ferie
+                            </tspan>
+                        </text>
+                    </g>
+                </g>
+                <g id="Group-6" transform="translate(13.000000, 0.000000)">
+                    <text
+                        id="Termin"
+                        fill="#3E3832"
+                        fontFamily="SourceSansPro-SemiBold, Source Sans Pro"
+                        fontWeight="400"
+                        fontSize="12">
+                        <tspan x="0.38" y="12">
+                            Termin
+                        </tspan>
+                    </text>
+                    <path
+                        id="Shape"
+                        d="M28.9333603,25.0418904 C26.1782938,22.3200859 21.6945154,22.3200859 18.9389053,25.0418904 C18.786148,25.1928034 18.6399142,25.3517723 18.5002039,25.5182599 C18.3604935,25.3512352 18.2142597,25.1928034 18.0609588,25.0413534 C15.3058923,22.3195489 10.8221139,22.3195489 8.06650377,25.0413534 C5.31089361,27.7631579 5.31143723,32.1928034 8.06650377,34.915145 L18.1153208,44.8426423 C18.2213268,44.9473684 18.3604935,45 18.4996602,45 C18.638827,45 18.7779937,44.9473684 18.8839996,44.8426423 L28.9328167,34.915145 C31.6889705,32.1933405 31.6889705,27.763695 28.9333603,25.0418904 Z"
+                        fill="#E86C60"
+                    />
+                </g>
+            </g>
+            <g
+                id="Group-4"
+                fill="#48433D"
+                fontFamily="SourceSansPro-SemiBold, Source Sans Pro"
+                fontSize="12"
+                transform="translate(83.000000, 78.000000)">
+                <g id="Group-2">
+                    <text id="Foreldrepenger-(utbe">
+                        <tspan x="0" y="12">
+                            Foreldrepenger (utbetaling)
+                        </tspan>
+                    </text>
+                </g>
+            </g>
+            <polygon
+                id="Triangle-Copy-4"
+                points="487.5 51 495 61 480 61"
+                stroke="#736E6A"
+                strokeWidth="0.5"
+                transform="translate(487.500000, 56.000000) rotate(180.000000) translate(-487.500000, -56.000000) "
+            />
+            <polygon
+                id="Triangle-Copy-5"
+                fill="#3E3832"
+                points="592.5 51 600 61 585 61"
+                stroke="#3E3832"
+                transform="translate(592.500000, 56.000000) rotate(180.000000) translate(-592.500000, -56.000000) "
+            />
+            <text
+                id="Opprinnelig-sluttdat"
+                fill="#736E6A"
+                fontFamily="SourceSansPro-SemiBoldItalic, Source Sans Pro"
+                fontStyle="italic"
+                fontSize="12">
+                <tspan x="458.204" y="29">
+                    Opprinnelig
+                </tspan>
+                <tspan x="464.996" y="44">
+                    sluttdato
+                </tspan>
+            </text>
+            <text
+                id="Ny-sluttdato"
+                fill="#3E3832"
+                fontFamily="SourceSansPro-Bold, Source Sans Pro"
+                fontSize="12">
+                <tspan x="585.384" y="29">
+                    Ny
+                </tspan>
+                <tspan x="568.338" y="44">
+                    sluttdato
+                </tspan>
+            </text>
+        </g>
+    </svg>
+);
+
+export default Ferieforskyvning;
