18caeed6916b46b992b3a8d06b470cc6520bf060
Kjetil Svalestuen <kjetil.svalestuen@bekk.no>
2018-10-08 13:08:05 +0200

    Lag en 'blindside' for lenker under arbeid

---
+++ src/app/pages/blindside/Blindside.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import Veilederpanel from 'nav-frontend-veilederpanel';
+import TypografiBase from 'nav-frontend-typografi';
+import { getTranslation, IntlProps, withIntl, Language } from '../../intl/intl';
+import CustomSVGFromSprite from '../../utils/CustomSVG';
+import BEMHelper from '../../utils/bem';
+import { withRouter } from 'react-router';
+import './blindside.less';
+import { VenstreChevron } from 'nav-frontend-chevron';
+import Sidebanner from '../../components/sidebanner/Sidebanner';
+
+const frida = require('../../assets/sara.svg').default;
+const cls = BEMHelper('blindside');
+
+interface Props {
+    history: any;
+}
+
+const Blindside: React.StatelessComponent<Props & IntlProps> = ({ history, lang }) => {
+    return (
+        <div className={cls.className}>
+            <Sidebanner text={getTranslation('blindside.tittel', lang)} />
+            <main className={cls.element('body')}>
+                <Tilbakeknapp goBack={history.goBack} />
+                <Veilederpanel
+                    svg={<CustomSVGFromSprite iconRef={frida} />}
+                    fargetema="normal"
+                    kompakt={true}>
+                    <TypografiBase type="normaltekst">
+                        {getTranslation('blindside.veileder', lang)}
+                    </TypografiBase>
+                </Veilederpanel>
+            </main>
+        </div>
+    );
+};
+
+const Tilbakeknapp = withIntl(({ goBack, lang }: { goBack: any; lang: Language }) => {
+    return (
+        <div
+            tabIndex={0}
+            className={cls.element('tilbake')}
+            onClick={() => {
+                goBack();
+            }}>
+            <VenstreChevron />
+            <TypografiBase type="element">{getTranslation('tilbake', lang)}</TypografiBase>
+        </div>
+    );
+});
+
+export default withRouter(withIntl(Blindside));
