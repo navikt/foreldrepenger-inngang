1d0e73b20dcb0348bd66c1eea0ed3fbf031534e0
Kjetil Svalestuen <kjetil.svalestuen@bekk.no>
2018-10-30 12:09:00 +0100

    Bruk footer-withmenu fra dekoratøren

--- src/build/scripts/decorator.js
+++ src/build/scripts/decorator.js
@@ -8,31 +8,23 @@ const requestDecorator = (callback) =>
     request(
         `${
             process.env.APPRES_CMS_URL
-        }/common-html/v4/navno?header-withmenu=true&styles=true&scripts=true&footer=true`,
+        }/common-html/v4/navno?header-withmenu=true&styles=true&scripts=true&footer-withmenu=true`,
         callback
     );
 
 const getDecorator = () =>
     new Promise((resolve, reject) => {
         const callback = (error, response, body) => {
-            if (
-                !error &&
-                response.statusCode >= 200 &&
-                response.statusCode < 400
-            ) {
+            if (!error && response.statusCode >= 200 && response.statusCode < 400) {
                 const { document } = new JSDOM(body).window;
                 const prop = 'innerHTML';
 
                 const data = {
                     NAV_SCRIPTS: document.getElementById('scripts')[prop],
                     NAV_STYLES: document.getElementById('styles')[prop],
-                    NAV_HEADING: document.getElementById('header-withmenu')[
-                        prop
-                    ],
-                    NAV_FOOTER: document.getElementById('footer')[prop],
-                    NAV_MENU_RESOURCES: document.getElementById(
-                        'megamenu-resources'
-                    )[prop]
+                    NAV_HEADING: document.getElementById('header-withmenu')[prop],
+                    NAV_FOOTER: document.getElementById('footer-withmenu')[prop],
+                    NAV_MENU_RESOURCES: document.getElementById('megamenu-resources')[prop]
                 };
                 resolve(data);
             } else {
