5d0228e451e3540821e2be040b769afcd2fc837f
ADEO\J154200 <Havard.Myrvold.Johannessen@nav.no>
2018-10-31 11:53:20 +0100

    far og far faner lagt til igjen

--- src/app/pages/om-foreldrepenger/hva-er-foreldrepenger/FarOgFar.tsx
+++ src/app/pages/om-foreldrepenger/hva-er-foreldrepenger/FarOgFar.tsx
@@ -9,17 +9,45 @@ import Foreldrepar from 'app/components/foreldrepar/Foreldrepar';
 
 const farOgFarContent = 'all-informasjon/hva-er-foreldrepenger/far-og-far/far-og-far';
 const kalkulatorbeskrivelse = 'all-informasjon/hva-er-foreldrepenger/kalkulatorbeskrivelse';
-const farOgFarFane = 'all-informasjon/hva-er-foreldrepenger/far-og-far/far-og-far-fane';
+
+const far1 = 'all-informasjon/hva-er-foreldrepenger/far-og-far/far-en';
+const far2 = 'all-informasjon/hva-er-foreldrepenger/far-og-far/far-to';
+const fellesPerioden = 'all-informasjon/hva-er-foreldrepenger/far-og-far/far-fellesperioden';
 
 const getInformasjonsfaner = (lang: Language) => [
+    {
+        faneLabel: getTranslation('om_foreldrepenger.hvor_lenge.fordeling.første_fedrekvote', lang),
+        faneIcon: true,
+        bodyProps: {
+            tittel: ' uker til hvis surrogati',
+            icon: 'far4',
+            antallUker: '45/56',
+            punktliste: [getTranslation('om_foreldrepenger.hvor_lenge.fordeling.krav.far1', lang)],
+            component: <StrukturertTekst tekst={getContent(far1, lang)} />
+        }
+    },
+    {
+        faneLabel: getTranslation('om_foreldrepenger.hvor_lenge.fordeling.andre_fedrekvote', lang),
+        faneIcon: true,
+        bodyProps: {
+            tittel: ' uker til hvis adopsjon',
+            icon: 'far2',
+            antallUker: '15',
+            punktliste: [getTranslation('om_foreldrepenger.hvor_lenge.fordeling.krav.far2', lang)],
+            component: <StrukturertTekst tekst={getContent(far2, lang)} />
+        }
+    },
     {
         faneLabel: getTranslation('om_foreldrepenger.hvor_lenge.fordeling.fellesperiode', lang),
         faneIcon: false,
         bodyProps: {
             tittel: '',
             icon: <Foreldrepar firstParent="far4" secondParent="far2" variant={4} />,
-            antallUker: '46/56',
-            component: <StrukturertTekst tekst={getContent(farOgFarFane, lang)} />
+            antallUker: '16/26 uker til hvis adopsjon',
+            punktliste: [
+                getTranslation('om_foreldrepenger.hvor_lenge.fordeling.krav.fellesPerioden', lang)
+            ],
+            component: <StrukturertTekst tekst={getContent(fellesPerioden, lang)} />
         }
     }
 ];
