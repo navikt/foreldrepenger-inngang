13a497ef1d0c0a7e58165d2326f17c4b4dcdec4a
ADEO\J154200 <Havard.Myrvold.Johannessen@nav.no>
2018-11-12 14:52:10 +0100

    fane komponent i valg klar

--- src/app/pages/veiviser/komponenter/valg/Valg.tsx
+++ src/app/pages/veiviser/komponenter/valg/Valg.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
 import BEMHelper from '../../../../utils/bem';
-import './valg.less';
-import StorToggle from '../../../../components/stor-toggle/StorToggle';
-import { FlexibleSvg } from '../../../../utils/CustomSVG';
 import { Language, getTranslation, withIntl } from '../../../../intl/intl';
 import TypografiBase from 'nav-frontend-typografi';
 import StrukturertTekst from '../../../../components/strukturert-tekst/StrukturertTekst';
 import { getContent } from '../../../../utils/getContent';
-
 import { RadioPanel } from 'nav-frontend-skjema';
-import Tab from "../tabs/Tab";
+import Tabs from 'nav-frontend-tabs';
+import { FlexibleSvg } from '../../../../utils/CustomSVG';
+import './valg.less';
 
-// import { RadioPanel } from 'nav-frontend-skjema';
 
 const cls = BEMHelper('valg');
-
-const far = require('../../../../assets/foreldre/far1.svg').default;
-const mor = require('../../../../assets/foreldre/mor2.svg').default;
-const medmor = require('../../../../assets/foreldre/medmor2.svg').default;
 
 const sprmalMor = [
     'veiviser.valg.spørsmål.mor.en',
@@ -59,69 +52,65 @@ const svarFarMedmor = [
     ]
 ];
 
-interface Tabs {
-    tabs: Tabs[];
+interface TabContent {
+    faner: any[];
 }
 
 interface Lang {
     lang: Language;
 }
 interface State {
-    parentToggled: boolean[]; // holder true/false state på tre øverste knappene
+    parentToggled: boolean[]; // holder state på parent-tabs.
     items: object[];
     valg: object[];
     checkbox: object[];
     teller: number;
     antallRader: number;
     numberofCheckBoxz: number;
-    test: boolean;
 }
 
-type Props = Lang & Tabs;
+type Props = Lang & TabContent;
 
 class Valg extends React.Component<Props, State> {
-    // setter toggled lik knapp som er valgt.
     toggled = null;
 
     constructor(props: Props) {
         super(props);
         this.state = {
             parentToggled: [true, true, true], // hvilke kort som er togglet
-            // hvilke checkbox som har blitt trykket på
-            checkbox: [
+            checkbox: [ // state for hvilke checkbox som er valg
                 [false, false],
                 [false, false],
                 [false, false],
                 [false, false],
                 [false, false]
             ],
-            valg: [], // valg listen som blir mappet i render
-            items: [],
+            valg: [], // valg : listen som blir mappet i render
+            items: [], //radiopanel list som blir mappet inne i valg list
             teller: 0, // teller for rad i checkbox
             antallRader: 0, // teller for antall rader i valg
             numberofCheckBoxz: 2, // antall checkbox som skal genereres pr rad,
-            test: false
         };
     }
 
-    updateToggle(nr: any): void {
-        if (this.toggled !== nr) {
+    updateToggle(e: any, nr: any): void {
+        if (this.toggled !== nr) { //hvis annen tab valgt
             this.toggled = nr;
-            let valgBytte = [...this.state.valg]; // lager kopi av bool value til brukerValg
+            let valgBytte = [...this.state.valg];
             valgBytte = [];
-            const tmp = [...this.state.parentToggled];
+            const tmpTabs = [...this.state.parentToggled];
             const noneChecked = [...this.state.checkbox];
             for (let i = 0; i < 3; i++) {
-                i === nr ? (tmp[i] = false) : (tmp[i] = true);
+                i === nr ? (tmpTabs[i] = false) : (tmpTabs[i] = true);
             }
             for (let j = 0; j < this.state.checkbox.length; j++) {
                 for (let k = 0; k < 2; k++) {
                     noneChecked[j][k] = false;
                 }
             }
-            this.setState(
+            this.setState(  //oppdaterer state /m nye verdier
                 {
-                    parentToggled: tmp,
+                    parentToggled: tmpTabs,
                     teller: 0,
                     valg: valgBytte,
                     checkbox: noneChecked,
@@ -181,20 +170,9 @@ class Valg extends React.Component<Props, State> {
     }
 
     checkRow(checkBoxNiva: number, svar: number, radNiva: number) {
-        console.log('//////////////////////////////////');
-        console.log('//////////////////////////////////');
-        console.log(
-            'checkbox lvl',
-            checkBoxNiva,
-            ' this.state.antallRader ',
-            this.state.antallRader,
-            ' radNiva',
-            radNiva
-        );
         const tmpItems = [...this.state.valg];
         const checked = [...this.state.checkbox];
         if (this.state.antallRader !== radNiva) {
-            // checkBoxNiva = radNiva;
             tmpItems.splice(radNiva + 1);
             for (let i = radNiva; i < this.state.checkbox.length; i++) {
                 for (let j = 0; j < 2; j++) {
@@ -210,11 +188,8 @@ class Valg extends React.Component<Props, State> {
     appendRow(checkBoxNiva: number, svar: number, radNiva: number) {
         // radNivå er rad-nr && svar er ja (0) / nei (1)
         const checked = [...this.state.checkbox];
-        console.log('checkBoxNiva :', checkBoxNiva, 'rad niva : ', radNiva, 'svar ', svar);
-        //// NIVA 1
-        if (checkBoxNiva === 0) {
+        if (checkBoxNiva === 0) {   // nivå 1
             if (svar === 0) {
-                // ****************************' SVAR : JA ***************************** //
                 checked[checkBoxNiva][svar] = true;
                 radNiva = 1;
                 checkBoxNiva = 3;
@@ -229,7 +204,6 @@ class Valg extends React.Component<Props, State> {
                         )
                 );
             } else if (svar === 1) {
-                // ****************************' SVAR : NEI ***************************** //
                 checked[checkBoxNiva][svar] = true;
                 radNiva = 1;
                 checkBoxNiva = 1;
@@ -370,73 +344,43 @@ class Valg extends React.Component<Props, State> {
     render = () => (
         <div className={cls.className}>
             <div className={cls.element('ingress')}>
-                {' '}
                 <StrukturertTekst
                     tekst={getContent('veiviser/header/header', this.props.lang)}
-                />{' '}
+                />
             </div>
             <div className={cls.element('forelder')}>
                 <div className={cls.element('ingress-knapp')}>
-                    <TypografiBase type="undertittel">
+                    <TypografiBase type="element">
                         {getTranslation('veiviser.ingress.knapper', this.props.lang)}
                     </TypografiBase>
                 </div>
                 <div className={cls.element('knapper')}>
-                    {this.props.tabs.map((tab, index) => (
-                        <Tab label={} icon={} lang={} setLanguage={}/>
-                        )}
-
-                    /*
-                    <StorToggle
-                        clsName={cls.element('knapp-frame')}
-                        isToggled={this.state.parentToggled[0]}
-                        onToggle={() => this.updateToggle(0)}>
-                        <div className={cls.element('knapp-element')}>
-                            <FlexibleSvg width={68} height={96} iconRef={far} />
-                            <div className={cls.element('label')}>
-                                <TypografiBase type="undertekstBold">
-                                    {getTranslation(
-                                        'veiviser.sidebanner.knappFar.label',
-                                        this.props.lang
-                                    )}
-                                </TypografiBase>
-                            </div>
-                        </div>
-                    </StorToggle>
-                    <StorToggle
-                        clsName={cls.element('knapp-frame')}
-                        isToggled={this.state.parentToggled[1]}
-                        onToggle={() => this.updateToggle(1)}>
-                        <div className={cls.element('knapp-element')}>
-                            <FlexibleSvg width={68} height={96} iconRef={mor} />
-                            <div className={cls.element('label')}>
-                                <TypografiBase type="undertekstBold">
-                                    {getTranslation(
-                                        'veiviser.sidebanner.knappMor.label',
-                                        this.props.lang
-                                    )}
-                                </TypografiBase>
-                            </div>
-                        </div>
-                    </StorToggle>
-                    <StorToggle
-                        clsName={cls.element('knapp-frame')}
-                        isToggled={this.state.parentToggled[2]}
-                        onToggle={() => this.updateToggle(2)}>
-                        <div className={cls.element('knapp-element')}>
-                            <FlexibleSvg width={68} height={96} iconRef={medmor} />
-                            <div className={cls.element('label')}>
-                                <TypografiBase type="undertekstBold">
-                                    {getTranslation(
-                                        'veiviser.sidebanner.knappMedmor.label',
-                                        this.props.lang
-                                    )}
-                                </TypografiBase>
-                            </div>
-                        </div>
-                    </StorToggle>
-                    */
-
+                    <Tabs
+                        kompakt={true}
+                        defaultAktiv={-1}
+                        tabs={this.props.faner.map((fane, index) => ({
+                            label: this.updateToggle[index],
+                            children: (
+                                <div className={cls.element('valg-fane')}>
+                                    <div className={cls.element('valg-fane-bakgrunn')}/>
+                                    <div className={cls.element('valg-fane-bilde')}>
+                                    <FlexibleSvg
+                                        iconRef={
+                                            require(`../../../../assets/foreldre/${fane.icon}.svg`).default
+                                        }
+                                        height={75}
+                                        width={50}
+                                    />
+                                    </div>
+                                    {fane.label}
+                                </div>
+                            ),
+                            onClick: this.updateToggle[index]
+                        }))}
+                        onChange={(event: any, index: number) => {
+                            this.updateToggle(event, index);
+                        }}
+                    />
                 </div>
             </div>
             <div className={cls.element('kort')}>
@@ -446,9 +390,7 @@ class Valg extends React.Component<Props, State> {
                         <div
                             key={valg.nr + Date.now()}
                             className={cls.element('kort-rad ' + valg.nr)}>
-                            <TypografiBase type="normaltekst">
-                            {valg.sprmal}
-                            </TypografiBase>
+                            <TypografiBase type="element">{valg.sprmal}</TypografiBase>
                             <div className={cls.element('valg-checkboxes')}>
                                 {valg.obj.map((item: any, index: number) => (
                                     <RadioPanel
