dcd53a73b19a4477ae4137a75ddeb53268760d3b
ADEO\J154200 <Havard.Myrvold.Johannessen@nav.no>
2018-11-28 13:21:24 +0100

    lagt til resultat banner for mobile && fix bugg på banner

--- src/app/pages/veiviser/komponenter/valg/komponenter/Logo.tsx
+++ src/app/pages/veiviser/komponenter/valg/komponenter/Logo.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import { withIntl } from '../../../../../intl/intl';
 import BEMHelper from '../../../../../utils/bem';
 import ResultatBilde from "./ResultatBilde";
+import MediaQuery from "react-responsive";
+import ResultatMobileBilde from "./ResultatMobileBilde";
+
 const cls = BEMHelper('valg');
 
-const Logo = ({width, height}: {width: string, height: string}) => {
+const Logo = () => {
     return (
         <div className={cls.element('resultat--stonad-bilde')}>
-      <ResultatBilde width={width} height={height}/>
+            <MediaQuery minWidth={576}>
+                <ResultatBilde/>
+            </MediaQuery>
+            <MediaQuery maxWidth={575}>
+                <ResultatMobileBilde/>
+            </MediaQuery>
         </div>
     );
 };
