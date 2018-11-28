8e67f189529ab589fe6963fc62e4da90acabe753
ADEO\J154200 <Havard.Myrvold.Johannessen@nav.no>
2018-11-28 15:48:10 +0100

    fiks resultat scaling

--- src/app/pages/veiviser/komponenter/valg/komponenter/ResultatBilde.tsx
+++ src/app/pages/veiviser/komponenter/valg/komponenter/ResultatBilde.tsx
@@ -5,7 +5,7 @@ const ResultatBilde = () => {
     return (
         <svg
             width={"100%"}
-            height={"auto"}
+            height={"100%"}
             viewBox="0 0 738 185"
             version="1.1"
             xmlns="http://www.w3.org/2000/svg"
--- src/app/pages/veiviser/komponenter/valg/komponenter/ResultatMobileBilde.tsx
+++ src/app/pages/veiviser/komponenter/valg/komponenter/ResultatMobileBilde.tsx
@@ -5,7 +5,7 @@ const ResultatMobileBilde = () => {
     return (
         <svg
             width="100%"
-            height="auto"
+            height="100%"
             viewBox="0 0 375 185"
             version="1.1"
             xmlns="http://www.w3.org/2000/svg"
