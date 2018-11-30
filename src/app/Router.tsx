5d7cdf3c0ba7d24d7a09992dc3923bc727dece23
Kjetil Svalestuen <kjetil.svalestuen@bekk.no>
2018-11-30 11:27:01 +0100

    Fjern ubrukte routes

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { Route, Redirect, Switch, withRouter } from 'react-router-dom';
 import Informasjonstavle from './pages/informasjonstavle/Informasjonstavle';
 import HvaSøkerDu from './pages/hva-søker-du/HvaSøkerDu';
-import SøkForeldrepenger from './pages/søk-foreldrepenger/SøkForeldrepenger';
 import SøkSvangerskapspenger from './pages/søk-svangerskapspenger/SøkSvangerskapspenger';
 import OmForeldrepenger from './pages/om-foreldrepenger/OmForeldrepenger';
 import OmEngangsstønad from './pages/om-engangsstønad/OmEngangsstønad';
@@ -14,15 +13,8 @@ import Veiviser from './pages/veiviser/Veiviser';
 const Router = () => (
     <Switch>
         <Route exact={true} path="/" component={Informasjonstavle} key="informasjonstavle" />
-        {/*<Route exact={true} path="/tjenester" component={Tjenester} key="tjenester" />*/}
         <Route exact={true} path="/hvor-mye" component={Kalkulator} key="kalkulator" />
         <Route exact={true} path="/hva-soker-du" component={HvaSøkerDu} key="hva-soker-du" />
-        <Route
-            exact={true}
-            path="/hva-soker-du/foreldrepenger"
-            component={SøkForeldrepenger}
-            key="foreldrepenger"
-        />
         <Route
             exact={true}
             path="/hva-soker-du/svangerskapspenger"
