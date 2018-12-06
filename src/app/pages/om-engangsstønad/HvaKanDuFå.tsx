157ee23c652a9328706af8604cd3c7d2f35f6531
Kjetil Svalestuen <kjetil.svalestuen@bekk.no>
2018-12-06 14:56:52 +0100

    Refaktorer om-engangsstønad

---
+++ src/app/pages/om-engangsstønad/HvaKanDuFå.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { ENGANGSSUM_PER_BARN } from '../../utils/beregningUtils';
+import { getContent } from 'app/utils/getContent';
+import { withIntl, IntlProps, getTranslation } from 'app/intl/intl';
+import PanelMedIllustrasjon from 'app/components/panel-med-illustrasjon/PanelMedIllustrasjon';
+import StrukturertTekst from 'app/components/strukturert-tekst/StrukturertTekst';
+import './omEngangsstønad.less';
+
+const arbeidstakerSvg = require('../../assets/ark/ark-arbeidstaker.svg').default;
+
+const HvaKanDuFå: React.StatelessComponent<IntlProps> = ({ lang }) => (
+    <PanelMedIllustrasjon
+        title={getTranslation('om_engangsstønad.hva_kan_du_få.tittel', lang)}
+        svg={arbeidstakerSvg}>
+        <StrukturertTekst
+            tekst={getContent('om-engangsstønad/hva-kan-du-få', lang)}
+            definisjoner={{
+                ENGANGSSUM_PER_BARN: ENGANGSSUM_PER_BARN.toLocaleString(lang)
+            }}
+        />
+    </PanelMedIllustrasjon>
+);
+
+export default withIntl(HvaKanDuFå);
---
+++ src/app/pages/om-engangsstønad/HvemKanFåEngangsstønad.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { getContent } from 'app/utils/getContent';
+import { withIntl, IntlProps, getTranslation } from 'app/intl/intl';
+import BEMHelper from 'app/utils/bem';
+import CustomSVGFromSprite from 'app/utils/CustomSVG';
+import PanelMedIllustrasjon from 'app/components/panel-med-illustrasjon/PanelMedIllustrasjon';
+import StrukturertTekst from 'app/components/strukturert-tekst/StrukturertTekst';
+import './omEngangsstønad.less';
+
+const cls = BEMHelper('omEngangsstønad');
+const engangsstønadSvg = require('../../assets/engangsstønad.svg').default;
+const checkmarkIcon = require('../../assets/icons/checkmark.svg').default;
+
+const hvaErEngangsstønadContent = 'om-engangsstønad/hvem-kan-få/hvem-kan-få';
+const engangssumContent = 'om-engangsstønad/hvem-kan-få/krav1';
+const utbetalingShortContent = 'om-engangsstønad/hvem-kan-få/krav2';
+
+const HvemKanFåEngangsstønad: React.StatelessComponent<IntlProps> = ({ lang }) => (
+    <PanelMedIllustrasjon
+        title={getTranslation('om_engangsstønad.hva_er.tittel', lang)}
+        svg={engangsstønadSvg}>
+        <StrukturertTekst tekst={getContent(hvaErEngangsstønadContent, lang)} />
+        <div className={cls.element('kravContainer')}>
+            <div className={cls.element('krav')}>
+                <CustomSVGFromSprite iconRef={checkmarkIcon} size={24} />
+                <StrukturertTekst tekst={getContent(engangssumContent, lang)} />
+            </div>
+            <div className={cls.element('krav')}>
+                <CustomSVGFromSprite iconRef={checkmarkIcon} size={24} />
+                <StrukturertTekst tekst={getContent(utbetalingShortContent, lang)} />
+            </div>
+        </div>
+    </PanelMedIllustrasjon>
+);
+
+export default withIntl(HvemKanFåEngangsstønad);
