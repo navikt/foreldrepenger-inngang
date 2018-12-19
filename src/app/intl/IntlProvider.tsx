b2c177612e415dedf5354dc7a4068ee7444538fb
Kjetil Svalestuen <kjetil.svalestuen@bekk.no>
2018-12-19 12:06:05 +0100

    Bruk språkvelgeren fra engangsstønad

--- src/app/intl/IntlProvider.tsx
+++ src/app/intl/IntlProvider.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { addLocaleData, IntlProvider as Provider } from 'react-intl';
+import * as en from 'react-intl/locale-data/en';
 import * as nb from 'react-intl/locale-data/nb';
 import * as nn from 'react-intl/locale-data/nn';
 
+import enMessages from './en_GB.json';
 import nnMessages from './nn_NO.json';
 import nbMessages from './nb_NO.json';
 
-export type Language = 'nb' | 'nn';
+export type Language = 'nb' | 'nn' | 'en';
 
 interface StateProps {
     language: Language;
@@ -16,11 +18,16 @@ class IntlProvider extends React.Component<StateProps> {
     constructor(props: StateProps) {
         super(props);
 
-        addLocaleData([...nb, ...nn]);
+        addLocaleData([...nb, ...nn, ...en]);
     }
 
     render() {
-        const messages = this.props.language === 'nb' ? nbMessages : nnMessages;
+        let messages = nbMessages;
+        if (this.props.language === 'en') {
+            messages = enMessages;
+        } else if (this.props.language === 'nn') {
+            messages = nnMessages;
+        }
 
         return (
             <Provider
