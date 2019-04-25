d6d76187089d1ba3a31ebc1d4cd56ecb87f0f8fd
Frode Hansen <frode.hansen@bekk.no>
2019-04-25 14:37:17 +0200

    Legge til info om ingen elektronisk id for selvstendig

--- src/app/pages/svangerskapspenger/slikSokerDu/faner/SelvstendigNaringsdrivendeEllerFrilanser.tsx
+++ src/app/pages/svangerskapspenger/slikSokerDu/faner/SelvstendigNaringsdrivendeEllerFrilanser.tsx
@@ -4,49 +4,79 @@ import Innhold, { getSource } from '../../../../utils/innhold/Innhold';
 import Sekvensliste from '../../../../components/sekvensliste/Sekvensliste';
 import Sekvens from '../../../../components/sekvensliste/Sekvens';
 import { FlexibleSvg } from '../../../../utils/CustomSVG';
+import UtvidetInformasjon from '../../../kalkulator/utvidetinformasjon/UtvidetInformasjon';
+import getTranslation from '../../../../utils/i18nUtils';
 
 const SelvstendigNaringsdrivendeEllerFrilanser: React.StatelessComponent<InjectedIntlProps> = ({
     intl
 }) => {
     return (
-        <Sekvensliste>
-            <Sekvens
-                illustrasjon={
-                    <FlexibleSvg
-                        iconRef={require(`../../../../assets/slik-soker-du/jordmor.svg`).default}
-                        height={96}
-                        width={96}
-                    />
-                }>
+        <>
+            <div style={{ marginBottom: '2rem' }}>
+                <Sekvensliste>
+                    <Sekvens
+                        illustrasjon={
+                            <FlexibleSvg
+                                iconRef={
+                                    require(`../../../../assets/slik-soker-du/jordmor.svg`).default
+                                }
+                                height={96}
+                                width={96}
+                            />
+                        }>
+                        <Innhold
+                            source={getSource(
+                                'svangerskapspenger/slik-soker-du/selvstendig.1',
+                                intl
+                            )}
+                        />
+                    </Sekvens>
+                    <Sekvens
+                        illustrasjon={
+                            <FlexibleSvg
+                                iconRef={
+                                    require(`../../../../assets/slik-soker-du/bilde.svg`).default
+                                }
+                                height={96}
+                                width={96}
+                            />
+                        }>
+                        <Innhold
+                            source={getSource(
+                                'svangerskapspenger/slik-soker-du/selvstendig.2',
+                                intl
+                            )}
+                        />
+                    </Sekvens>
+                    <Sekvens
+                        illustrasjon={
+                            <FlexibleSvg
+                                iconRef={
+                                    require(`../../../../assets/slik-soker-du/soke.svg`).default
+                                }
+                                height={96}
+                                width={96}
+                            />
+                        }>
+                        <Innhold
+                            source={getSource(
+                                'svangerskapspenger/slik-soker-du/selvstendig.3',
+                                intl
+                            )}
+                        />
+                    </Sekvens>
+                </Sekvensliste>
+            </div>
+            <UtvidetInformasjon
+                apneLabel={getTranslation(
+                    'info.svangerskapspenger.harIkkeElektroniskID.tittel',
+                    intl
+                )}>
                 <Innhold
-                    source={getSource('svangerskapspenger/slik-soker-du/selvstendig.1', intl)}
+                    source={getSource('svangerskapspenger/slik-soker-du/elektroniskId', intl)}
                 />
-            </Sekvens>
-            <Sekvens
-                illustrasjon={
-                    <FlexibleSvg
-                        iconRef={require(`../../../../assets/slik-soker-du/bilde.svg`).default}
-                        height={96}
-                        width={96}
-                    />
-                }>
-                <Innhold
-                    source={getSource('svangerskapspenger/slik-soker-du/selvstendig.2', intl)}
-                />
-            </Sekvens>
-            <Sekvens
-                illustrasjon={
-                    <FlexibleSvg
-                        iconRef={require(`../../../../assets/slik-soker-du/soke.svg`).default}
-                        height={96}
-                        width={96}
-                    />
-                }>
-                <Innhold
-                    source={getSource('svangerskapspenger/slik-soker-du/selvstendig.3', intl)}
-                />
-            </Sekvens>
-        </Sekvensliste>
+            </UtvidetInformasjon>
+        </>
     );
 };
 
