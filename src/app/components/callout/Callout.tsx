50b1625af1e36616f6dbc567f8a41308d0287b5c
Frode Hansen <frode.hansen@bekk.no>
2019-04-25 10:50:39 +0200

    Callout of sekvensliste update

--- src/app/components/callout/Callout.tsx
+++ src/app/components/callout/Callout.tsx
@@ -1,12 +1,18 @@
 import * as React from 'react';
 import './callout.less';
+import { Normaltekst } from 'nav-frontend-typografi';
 
 interface Props {
     children: React.ReactNode;
 }
 
 const Callout: React.StatelessComponent<Props> = ({ children }) => (
-    <div className="callout">{children}</div>
+    <div className="callout">
+        <div className="callout__arrowWrapper" role="presentation" aria-hidden={true}>
+            <div className="callout__arrow" />
+        </div>
+        <Normaltekst tag="div">{children}</Normaltekst>
+    </div>
 );
 
 export default Callout;
