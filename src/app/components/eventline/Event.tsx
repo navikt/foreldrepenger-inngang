0d11bb198912b11878e69627a86afb064c8be1b1
Andreas Dybdahl <andreas.dyb@gmail.com>
2019-06-04 13:07:13 +0200

    Lagt til eventlinje

---
+++ src/app/components/eventline/Event.tsx
@@ -0,0 +1,25 @@
+import React, { FunctionComponent } from 'react';
+import BEMHelper from 'app/utils/bem';
+
+import './eventline.less';
+import { Undertittel } from 'nav-frontend-typografi';
+
+const cls = BEMHelper('eventline');
+
+interface Props {
+    children: React.ReactNode;
+    title?: string;
+}
+
+const Event: FunctionComponent<Props> = ({ children, title }) => {
+    return (
+        <div className={cls.element('event')}>
+            {title !== undefined && (
+                <Undertittel className={cls.element('eventTitle')}>{title}</Undertittel>
+            )}
+            <div className={cls.element('eventContent')}>{children}</div>
+        </div>
+    );
+};
+
+export default Event;
--- src/app/pages/søk-foreldrepenger/InfoFarOgMor.tsx
+++ src/app/pages/søk-foreldrepenger/InfoFarOgMor.tsx
@@ -1,6 +1,8 @@
 import React, { FunctionComponent } from 'react';
 import { InjectedIntl, injectIntl } from 'react-intl';
 import EkspanderbarSnakkeboble from 'app/components/ekspanderbar-snakkeboble/EkspanderbarSnakkeboble';
+import Eventline from 'app/components/eventline/Eventline';
+import Event from 'app/components/eventline/Event';
 
 interface Props {
     intl: InjectedIntl;
@@ -10,28 +12,29 @@ const InfoFarOgMor: FunctionComponent<Props> = ({ intl }) => {
     const svg = require('../../components/panel-med-bilde/img/hvor-lenge.svg').default;
 
     return (
-        <div>
-            <EkspanderbarSnakkeboble
-                svg={svg}
-                tittel="Planlegg tiden hjemme med barnet"
-                innhold="søk-foreldrepenger/planlegg"
-            />
-            <EkspanderbarSnakkeboble
-                svg={svg}
-                tittel="Planlegg tiden hjemme med barnet"
-                innhold="søk-foreldrepenger/planlegg"
-            />
-            <EkspanderbarSnakkeboble
-                svg={svg}
-                tittel="Planlegg tiden hjemme med barnet"
-                innhold="søk-foreldrepenger/planlegg"
-            />
-            <EkspanderbarSnakkeboble
-                svg={svg}
-                tittel="Planlegg tiden hjemme med barnet"
-                innhold="søk-foreldrepenger/planlegg"
-            />
-        </div>
+        <Eventline>
+            <Event title="Dere venter barn">
+                <EkspanderbarSnakkeboble
+                    svg={svg}
+                    tittel="Planlegg tiden hjemme med barnet"
+                    innhold="søk-foreldrepenger/planlegg"
+                />
+            </Event>
+            <Event title="22. svangerskapsuke">
+                <EkspanderbarSnakkeboble
+                    svg={svg}
+                    tittel="Planlegg tiden hjemme med barnet"
+                    innhold="søk-foreldrepenger/planlegg"
+                />
+            </Event>
+            <Event title="4 uker før mors første dag med foreldrepenger">
+                <EkspanderbarSnakkeboble
+                    svg={svg}
+                    tittel="Planlegg tiden hjemme med barnet"
+                    innhold="søk-foreldrepenger/planlegg"
+                />
+            </Event>
+        </Eventline>
     );
 };
 
