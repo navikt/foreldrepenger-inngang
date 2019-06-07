2d34e4ed15619ad323478724100a8d99aeb72b85
Andreas Dybdahl <andreas.dyb@gmail.com>
2019-06-07 13:09:06 +0200

    Informasjon for far og mor scenario

--- src/app/pages/søk-foreldrepenger/InfoFarOgMor.tsx
+++ src/app/pages/søk-foreldrepenger/InfoFarOgMor.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
-import { InjectedIntl, FormattedHTMLMessage, injectIntl } from 'react-intl';
+import { InjectedIntl, injectIntl } from 'react-intl';
 import EkspanderbarSnakkeboble from 'app/components/ekspanderbar-snakkeboble/EkspanderbarSnakkeboble';
 import Eventline from 'app/components/eventline/Eventline';
 import Event from 'app/components/eventline/Event';
@@ -25,38 +25,50 @@ const InfoFarOgMor: FunctionComponent<Props> = ({ intl }) => {
             </Event>
             <Event title="22. svangerskapsuke">
                 <EkspanderbarSnakkeboble svg={morBrev} tittel="Mor kan søke om foreldrepenger">
-                    <FormattedHTMLMessage id="søk_foreldrepenger.far_og_mor.dere_venter_barn.paragraf_1" />
-                    <FormattedHTMLMessage id="søk_foreldrepenger.far_og_mor.dere_venter_barn.paragraf_2" />
+                    <Innhold
+                        source={getSource('søk-foreldrepenger/far-og-mor/mor-kan-søke', intl)}
+                    />
                 </EkspanderbarSnakkeboble>
             </Event>
             <Event title="4 uker før mors første dag med foreldrepenger">
                 <EkspanderbarSnakkeboble
                     svg={inntektsOpplysninger}
                     tittel="Vi får opplysninger om inntekten til mor">
-                    <FormattedHTMLMessage id="søk_foreldrepenger.far_og_mor.dere_venter_barn.paragraf_1" />
-                    <FormattedHTMLMessage id="søk_foreldrepenger.far_og_mor.dere_venter_barn.paragraf_2" />
+                    <Innhold
+                        source={getSource(
+                            'søk-foreldrepenger/far-og-mor/opplysninger-inntekt-mor',
+                            intl
+                        )}
+                    />
                 </EkspanderbarSnakkeboble>
                 <EkspanderbarSnakkeboble svg={fåSvar} tittel="Mor får svart på søknaden">
-                    <FormattedHTMLMessage id="søk_foreldrepenger.far_og_mor.dere_venter_barn.paragraf_1" />
-                    <FormattedHTMLMessage id="søk_foreldrepenger.far_og_mor.dere_venter_barn.paragraf_2" />
+                    <Innhold
+                        source={getSource('søk-foreldrepenger/far-og-mor/mor-får-svar', intl)}
+                    />
                 </EkspanderbarSnakkeboble>
             </Event>
             <Event title="Barnet blir født">
                 <EkspanderbarSnakkeboble svg={farBrev} tittel="Far kan søke om foreldrepenger">
-                    <FormattedHTMLMessage id="søk_foreldrepenger.far_og_mor.dere_venter_barn.paragraf_1" />
-                    <FormattedHTMLMessage id="søk_foreldrepenger.far_og_mor.dere_venter_barn.paragraf_2" />
+                    <Innhold
+                        source={getSource('søk-foreldrepenger/far-og-mor/far-kan-søke', intl)}
+                    />
                 </EkspanderbarSnakkeboble>
             </Event>
             <Event title="4 uker før fars første dag med foreldrepenger">
                 <EkspanderbarSnakkeboble
                     svg={inntektsOpplysninger}
                     tittel="Inntektsopplysningene til far kan komme inn">
-                    <FormattedHTMLMessage id="søk_foreldrepenger.far_og_mor.dere_venter_barn.paragraf_1" />
-                    <FormattedHTMLMessage id="søk_foreldrepenger.far_og_mor.dere_venter_barn.paragraf_2" />
+                    <Innhold
+                        source={getSource(
+                            'søk-foreldrepenger/far-og-mor/opplysninger-inntekt-far',
+                            intl
+                        )}
+                    />
                 </EkspanderbarSnakkeboble>
                 <EkspanderbarSnakkeboble svg={fåSvar} tittel="Far kan få svar på søknaden">
-                    <FormattedHTMLMessage id="søk_foreldrepenger.far_og_mor.dere_venter_barn.paragraf_1" />
-                    <FormattedHTMLMessage id="søk_foreldrepenger.far_og_mor.dere_venter_barn.paragraf_2" />
+                    <Innhold
+                        source={getSource('søk-foreldrepenger/far-og-mor/far-får-svar', intl)}
+                    />
                 </EkspanderbarSnakkeboble>
             </Event>
         </Eventline>
