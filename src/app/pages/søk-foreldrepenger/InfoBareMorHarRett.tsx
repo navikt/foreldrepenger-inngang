5586418dfdf751967847a51ab16701f1b3d2b5cb
Andreas Dybdahl <andreas.dyb@gmail.com>
2019-06-07 13:36:34 +0200

    Info for bare mor har rett scenario

--- src/app/pages/søk-foreldrepenger/InfoBareMorHarRett.tsx
+++ src/app/pages/søk-foreldrepenger/InfoBareMorHarRett.tsx
@@ -1,44 +1,61 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import Eventline from 'app/components/eventline/Eventline';
-// import Event from 'app/components/eventline/Event';
-// import EkspanderbarSnakkeboble from 'app/components/ekspanderbar-snakkeboble/EkspanderbarSnakkeboble';
+import Event from 'app/components/eventline/Event';
+import EkspanderbarSnakkeboble from 'app/components/ekspanderbar-snakkeboble/EkspanderbarSnakkeboble';
+import Innhold, { getSource } from 'app/utils/innhold/Innhold';
+import { injectIntl, InjectedIntl } from 'react-intl';
 
-const InfoBareMorHarRett = () => {
-    // const kalender = require('../../assets/planlegge.svg').default;
-    // const morBrev = require('../../assets/foreldre/mor1-brev.svg').default;
-    // const inntektsOpplysninger = require('../../assets/inntektsopplysninger.svg').default;
-    // const fåSvar = require('../../assets/faa-svar.svg').default;
+interface Props {
+    intl: InjectedIntl;
+}
+
+const InfoBareMorHarRett: FunctionComponent<Props> = ({ intl }) => {
+    const kalender = require('../../assets/planlegge.svg').default;
+    const morBrev = require('../../assets/foreldre/mor2-brev.svg').default;
+    const inntektsOpplysninger = require('../../assets/inntektsopplysninger.svg').default;
+    const fåSvar = require('../../assets/faa-svar.svg').default;
 
     return (
         <Eventline>
-            {/* <Event title="Dere venter barn">
-                <EkspanderbarSnakkeboble
-                    svg={kalender}
-                    tittel="Planlegg tiden hjemme med barnet"
-                    innhold="søk-foreldrepenger/planlegg"
-                />
+            <Event title="Dere venter barn">
+                <EkspanderbarSnakkeboble svg={kalender} tittel="Planlegg tiden hjemme med barnet">
+                    <Innhold
+                        source={getSource('søk-foreldrepenger/bare-mor-har-rett/planlegg', intl)}
+                    />
+                </EkspanderbarSnakkeboble>
             </Event>
             <Event title="22. svangerskapsuke">
-                <EkspanderbarSnakkeboble
-                    svg={morBrev}
-                    tittel="Mor kan søke om foreldrepenger"
-                    innhold="søk-foreldrepenger/planlegg"
-                />
+                <EkspanderbarSnakkeboble svg={morBrev} tittel="Mor kan søke om foreldrepenger">
+                    <Innhold
+                        source={getSource(
+                            'søk-foreldrepenger/bare-mor-har-rett/mor-kan-søke',
+                            intl
+                        )}
+                    />
+                </EkspanderbarSnakkeboble>
             </Event>
             <Event title="4 uker før mors første dag med foreldrepenger">
                 <EkspanderbarSnakkeboble
                     svg={inntektsOpplysninger}
-                    tittel="Vi får opplysninger om inntekten til mor"
-                    innhold="søk-foreldrepenger/planlegg"
-                />
-                <EkspanderbarSnakkeboble
-                    svg={fåSvar}
-                    tittel="Mor får svart på søknaden"
-                    innhold="søk-foreldrepenger/planlegg"
-                />
-            </Event> */}
+                    tittel="Vi får opplysninger om inntekten til mor">
+                    <Innhold
+                        source={getSource(
+                            'søk-foreldrepenger/bare-mor-har-rett/opplysninger-inntekt-mor',
+                            intl
+                        )}
+                    />
+                </EkspanderbarSnakkeboble>
+                <EkspanderbarSnakkeboble svg={fåSvar} tittel="Mor får svart på søknaden">
+                    <Innhold
+                        source={getSource(
+                            'søk-foreldrepenger/bare-mor-har-rett/mor-får-svar',
+                            intl
+                        )}
+                    />
+                </EkspanderbarSnakkeboble>
+            </Event>
         </Eventline>
     );
 };
 
-export default InfoBareMorHarRett;
+export default injectIntl(InfoBareMorHarRett);
