91da3874eae98eb3f82b576f7adac38610c1a9ec
Andreas Dybdahl <andreas.dyb@gmail.com>
2019-06-07 14:35:49 +0200

    Info for far og far scenario

--- src/app/pages/søk-foreldrepenger/InfoFarOgFar.tsx
+++ src/app/pages/søk-foreldrepenger/InfoFarOgFar.tsx
@@ -1,60 +1,100 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import Eventline from 'app/components/eventline/Eventline';
-// import Event from 'app/components/eventline/Event';
-// import EkspanderbarSnakkeboble from 'app/components/ekspanderbar-snakkeboble/EkspanderbarSnakkeboble';
+import Event from 'app/components/eventline/Event';
+import EkspanderbarSnakkeboble from 'app/components/ekspanderbar-snakkeboble/EkspanderbarSnakkeboble';
+import { InjectedIntl, injectIntl } from 'react-intl';
+import Innhold, { getSource } from 'app/utils/innhold/Innhold';
 
-const InfoFarOgFar = () => {
-    // const svg = require('../../components/panel-med-bilde/img/hvor-lenge.svg').default;
+interface Props {
+    intl: InjectedIntl;
+}
+
+const InfoFarOgFar: FunctionComponent<Props> = ({ intl }) => {
+    const kalender = require('../../assets/planlegge.svg').default;
+    const dokumentasjon = require('../../assets/dokumentasjon.svg').default;
+    const far1Brev = require('../../assets/foreldre/far2-brev.svg').default;
+    const inntektsOpplysninger = require('../../assets/inntektsopplysninger.svg').default;
+    const fåSvar = require('../../assets/faa-svar.svg').default;
+    const far2Brev = require('../../assets/foreldre/far3-brev.svg').default;
 
     return (
-        <Eventline>
-            {/* <Event title="Dere venter barn">
-                <EkspanderbarSnakkeboble
-                    svg={svg}
-                    tittel="Planlegg tiden hjemme med barnet"
-                    innhold="søk-foreldrepenger/planlegg"
-                />
-            </Event>
-            <Event title="22. svangerskapsuke">
-                <EkspanderbarSnakkeboble
-                    svg={svg}
-                    tittel="Mor kan søke om foreldrepenger"
-                    innhold="søk-foreldrepenger/planlegg"
-                />
-            </Event>
-            <Event title="4 uker før mors første dag med foreldrepenger">
-                <EkspanderbarSnakkeboble
-                    svg={svg}
-                    tittel="Vi får opplysninger om inntekten til mor"
-                    innhold="søk-foreldrepenger/planlegg"
-                />
-                <EkspanderbarSnakkeboble
-                    svg={svg}
-                    tittel="Mor får svart på søknaden"
-                    innhold="søk-foreldrepenger/planlegg"
-                />
-            </Event>
-            <Event title="Barnet blir født">
-                <EkspanderbarSnakkeboble
-                    svg={svg}
-                    tittel="Far kan søke om foreldrepenger"
-                    innhold="søk-foreldrepenger/planlegg"
-                />
-            </Event>
-            <Event title="4 uker før fars første dag med foreldrepenger">
-                <EkspanderbarSnakkeboble
-                    svg={svg}
-                    tittel="Inntektsopplysningene til far kan komme inn"
-                    innhold="søk-foreldrepenger/planlegg"
-                />
-                <EkspanderbarSnakkeboble
-                    svg={svg}
-                    tittel="Far kan få svar på søknaden"
-                    innhold="søk-foreldrepenger/planlegg"
-                />
-            </Event> */}
-        </Eventline>
+        <>
+            <Eventline>
+                <Event title="Dere venter barn">
+                    <EkspanderbarSnakkeboble
+                        svg={kalender}
+                        tittel="Planlegg tiden hjemme med barnet">
+                        <Innhold
+                            source={getSource('søk-foreldrepenger/far-og-far/planlegg', intl)}
+                        />
+                    </EkspanderbarSnakkeboble>
+                </Event>
+                <Event title="Dere får vite dato for termin eller omsorgsovertakelse">
+                    <EkspanderbarSnakkeboble
+                        svg={dokumentasjon}
+                        tittel="Dere får dokumentasjon på omsorgsovertakelse">
+                        <Innhold
+                            source={getSource('søk-foreldrepenger/far-og-far/dokumentasjon', intl)}
+                        />
+                    </EkspanderbarSnakkeboble>
+                    <EkspanderbarSnakkeboble
+                        svg={far1Brev}
+                        tittel="Far 1 kan søke om foreldrepenger">
+                        <Innhold
+                            source={getSource('søk-foreldrepenger/far-og-far/far-kan-søke', intl)}
+                        />
+                    </EkspanderbarSnakkeboble>
+                </Event>
+                <Event title="4 uker før far 1 sin første dag med foreldrepenger">
+                    <EkspanderbarSnakkeboble
+                        svg={inntektsOpplysninger}
+                        tittel="Vi får opplysninger om inntekten til far 1">
+                        <Innhold
+                            source={getSource(
+                                'søk-foreldrepenger/far-og-far/opplysninger-inntekt-far',
+                                intl
+                            )}
+                        />
+                    </EkspanderbarSnakkeboble>
+                    <EkspanderbarSnakkeboble svg={fåSvar} tittel="Far 1 får svar på søknaden">
+                        <Innhold
+                            source={getSource('søk-foreldrepenger/far-og-far/far-får-svar', intl)}
+                        />
+                    </EkspanderbarSnakkeboble>
+                    <EkspanderbarSnakkeboble
+                        svg={far2Brev}
+                        tittel="Far 2 kan søke om foreldrepenger">
+                        <Innhold
+                            source={getSource(
+                                'søk-foreldrepenger/far-og-far/medfar-kan-søke',
+                                intl
+                            )}
+                        />
+                    </EkspanderbarSnakkeboble>
+                </Event>
+                <Event title="4 uker før far 2 sin første dag med foreldrepenger">
+                    <EkspanderbarSnakkeboble
+                        svg={inntektsOpplysninger}
+                        tittel="Vi får opplysninger om inntekten til far 2">
+                        <Innhold
+                            source={getSource(
+                                'søk-foreldrepenger/far-og-far/opplysninger-inntekt-medfar',
+                                intl
+                            )}
+                        />
+                    </EkspanderbarSnakkeboble>
+                    <EkspanderbarSnakkeboble svg={fåSvar} tittel="Far 2 får svar på søknaden">
+                        <Innhold
+                            source={getSource(
+                                'søk-foreldrepenger/far-og-far/medfar-får-svar',
+                                intl
+                            )}
+                        />
+                    </EkspanderbarSnakkeboble>
+                </Event>
+            </Eventline>
+        </>
     );
 };
 
-export default InfoFarOgFar;
+export default injectIntl(InfoFarOgFar);
