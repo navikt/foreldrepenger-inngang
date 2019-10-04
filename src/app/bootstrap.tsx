de9c2d4ab3de6f248035750c10e5ddcc7a38daab
Odd Einar Hoel <odd.einar.hoel@nav.no>
2019-10-04 13:41:34 +0200

    Add sentry setup

--- src/app/bootstrap.tsx
+++ src/app/bootstrap.tsx
@@ -3,19 +3,29 @@ import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ScrollToTop from './utils/ScrollToTop';
 import App from './App';
+import * as Sentry from '@sentry/browser';
+import { Normaltekst } from 'nav-frontend-typografi';
+import ErrorBoundary from './components/error-boundary/ErrorBoundary';
 
 import './styles/index.less';
-import { Normaltekst } from 'nav-frontend-typografi';
 
 const root = document.getElementById('app');
 
+Sentry.init({
+    dsn: 'https://d3f3fc7e51f24438bdfb44c9ca3cd9bc@sentry.nav.no/19',
+    environment: window.location.hostname,
+    integrations: [new Sentry.Integrations.Breadcrumbs({ console: false })]
+});
+
 render(
-    <Router>
-        <ScrollToTop>
-            <Normaltekst tag="div">
-                <App />
-            </Normaltekst>
-        </ScrollToTop>
-    </Router>,
+    <ErrorBoundary>
+        <Router>
+            <ScrollToTop>
+                <Normaltekst tag="div">
+                    <App />
+                </Normaltekst>
+            </ScrollToTop>
+        </Router>
+    </ErrorBoundary>,
     root
 );
