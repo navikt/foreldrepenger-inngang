5aecb83ad07572bab883b32f50c3f25142a56ec0
Odd Einar Hoel <odd.einar.hoel@nav.no>
2019-10-21 11:21:49 +0200

    Delete unused env variable

--- src/build/scripts/envSettings.js
+++ src/build/scripts/envSettings.js
@@ -9,7 +9,6 @@ function createEnvSettingsFile(settingsFile) {
                 SOK_FORELDREPENGER_PAPIR_URL: '${process.env.SOK_FORELDREPENGER_PAPIR_URL}',
                 SOK_ENGANGSSTONAD_URL: '${process.env.SOK_ENGANGSSTONAD_URL}',
                 SOK_ENGANGSSTONAD_PAPIR_URL: '${process.env.SOK_ENGANGSSTONAD_PAPIR_URL}',
-                ENDRE_SOKNAD_FORELDREPENGER_URL: '${process.env.ENDRE_SOKNAD_FORELDREPENGER_URL}',
                 DINE_FORELDREPENGER_URL: '${process.env.DINE_FORELDREPENGER_URL}',
                 SOK_SVANGERSKAPSPENGER_URL: '${process.env.SOK_SVANGERSKAPSPENGER_URL}',
                 SOK_SVANGERSKAPSPENGER_PAPIR_URL: '${process.env.SOK_SVANGERSKAPSPENGER_PAPIR_URL}',
