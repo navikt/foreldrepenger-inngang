441ebca10f1e51a61068f42651bd9cf3a2e8f415
Andreas Dybdahl <andreas.dybdahl@nav.no>
2020-03-06 10:04:53 +0100

    Lagt til info for fiskere også i svangerskapspengesiden

--- src/app/pages/svangerskapspenger/hvorMyeKanDuFa/HvorMyeKanDuFa.tsx
+++ src/app/pages/svangerskapspenger/hvorMyeKanDuFa/HvorMyeKanDuFa.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { InjectedIntlProps, injectIntl } from 'react-intl';
+import { InjectedIntlProps, injectIntl, InjectedIntl } from 'react-intl';
 import PanelMedIllustrasjon from '../../../components/panel-med-illustrasjon/PanelMedIllustrasjon';
 import getTranslation from '../../../utils/i18nUtils';
 import Innhold, { getSource } from '../../../utils/innhold/Innhold';
@@ -11,47 +11,53 @@ import Arbeidstaker from './faner/Arbeidstaker';
 import Frilanser from './faner/Frilanser';
 import SelvStendigNaring from './faner/SelvStendigNaring';
 import Innholdsfaner from '../../../components/innholdsfaner/Innholdsfaner';
+import FiskerIkon from 'app/pages/om-foreldrepenger/beregning/ikoner/FiskerIkon';
 
 const cls = BEMHelper('hvorMyeKanDuFa');
 const seksjonsBilde = require('../../../assets/ark/ark-beregning.svg').default;
 
 interface Props {
-    id: string;
+  id: string;
 }
 
-const faner = [
-    {
-        label: 'om_svangerskapspenger.hvorMyeKanDuFa.faneTittel.arbeidstaker',
-        icon: <ArbeidstakerIkon />,
-        component: <Arbeidstaker />
-    },
-    {
-        label: 'om_svangerskapspenger.hvorMyeKanDuFa.faneTittel.frilanser',
-        icon: <FrilanserIkon />,
-        component: <Frilanser />
-    },
-    {
-        label: 'om_svangerskapspenger.hvorMyeKanDuFa.faneTittel.selvstendignaring',
-        icon: <SelvstendigIkon />,
-        component: <SelvStendigNaring />
-    }
+const getFaner = (intl: InjectedIntl) => [
+  {
+    label: 'om_svangerskapspenger.hvorMyeKanDuFa.faneTittel.arbeidstaker',
+    icon: <ArbeidstakerIkon />,
+    component: <Arbeidstaker />
+  },
+  {
+    label: 'om_svangerskapspenger.hvorMyeKanDuFa.faneTittel.frilanser',
+    icon: <FrilanserIkon />,
+    component: <Frilanser />
+  },
+  {
+    label: 'om_svangerskapspenger.hvorMyeKanDuFa.faneTittel.selvstendignaring',
+    icon: <SelvstendigIkon />,
+    component: <SelvStendigNaring />
+  },
+  {
+    label: 'om_foreldrepenger.beregning.fisker',
+    icon: <FiskerIkon />,
+    component: <Innhold source={getSource('om-foreldrepenger/beregning/fisker', intl)} />
+  }
 ];
 
 const HvorMyeKanDuFa: React.StatelessComponent<Props & InjectedIntlProps> = ({ id, intl }) => {
-    return (
-        <PanelMedIllustrasjon
-            id={id}
-            className={cls.block}
-            title={getTranslation('om_svangerskapspenger.hvorMyeKanDuFa.tittel', intl)}
-            svg={seksjonsBilde}>
-            <Innhold
-                source={getSource('svangerskapspenger/hvor-mye-kan-du-fa/informasjonstekst', intl)}
-            />
-            <div className={cls.element('alignLeft')}>
-                <Innholdsfaner tabs={faner} />
-            </div>
-        </PanelMedIllustrasjon>
-    );
+  return (
+    <PanelMedIllustrasjon
+      id={id}
+      className={cls.block}
+      title={getTranslation('om_svangerskapspenger.hvorMyeKanDuFa.tittel', intl)}
+      svg={seksjonsBilde}>
+      <Innhold
+        source={getSource('svangerskapspenger/hvor-mye-kan-du-fa/informasjonstekst', intl)}
+      />
+      <div className={cls.element('alignLeft')}>
+        <Innholdsfaner tabs={getFaner(intl)} />
+      </div>
+    </PanelMedIllustrasjon>
+  );
 };
 
 export default injectIntl(HvorMyeKanDuFa);
