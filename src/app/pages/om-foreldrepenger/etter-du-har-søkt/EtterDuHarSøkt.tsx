b1f13406774e462be50627b5fddd677451e9f51c
Andreas Dybdahl <andreas.dybdahl@nav.no>
2020-03-20 14:11:15 +0100

    Lagt til nytt fakta ark for foreldrepenger

--- src/app/pages/om-foreldrepenger/etter-du-har-søkt/EtterDuHarSøkt.tsx
+++ src/app/pages/om-foreldrepenger/etter-du-har-søkt/EtterDuHarSøkt.tsx
@@ -1,23 +1,11 @@
 import * as React from 'react';
-import { detErJul } from 'app/utils/datoUtils';
-
 import { injectIntl, InjectedIntlProps } from 'react-intl';
-import BEMHelper from '../../../utils/bem';
+
 import getTranslation from 'app/utils/i18nUtils';
 import PanelMedIllustrasjon from '../../../components/panel-med-illustrasjon/PanelMedIllustrasjon';
 import Innhold, { getSource } from 'app/utils/innhold/Innhold';
-import SvgMask from 'app/components/svg-mask/SvgMask';
 
-const cls = BEMHelper('hvemKanFåForeldrepenger');
-const foreldrepengerSvg = detErJul()
-    ? require('../../../assets/familier/familie-sesong-1.svg').default
-    : require('../../../assets/familier/familie-1.svg').default;
-
-const kravTilForeldrepenger = [
-    'om-foreldrepenger/hvem-kan-få/krav1',
-    'om-foreldrepenger/hvem-kan-få/krav2',
-    'om-foreldrepenger/hvem-kan-få/krav3'
-];
+const statusSVG = require('../../../assets/ark/ark-saksstatus.svg').default;
 
 interface Props {
     id: string;
@@ -27,12 +15,11 @@ const EtterDuHarSøkt: React.StatelessComponent<Props & InjectedIntlProps> = ({ 
     return (
         <PanelMedIllustrasjon
             id={id}
-            title={getTranslation('om_foreldrepenger.for_å_få.tittel', intl)}
-            svg={<SvgMask svg={foreldrepengerSvg} anchorToBottom={true} />}>
-            <Innhold source={getSource('om-foreldrepenger/hvem-kan-få/ingress', intl)} />
-            <div className={cls.element('kravTilForeldrepenger')}>
-                {kravTilForeldrepenger.map((krav) => krav)}
-            </div>
+            title={getTranslation('om_foreldrepenger.etter_du_har_søkt.tittel', intl)}
+            svg={statusSVG}>
+            <Innhold
+                source={getSource('om-foreldrepenger/etter-du-har-søkt/etter-du-har-søkt', intl)}
+            />
         </PanelMedIllustrasjon>
     );
 };
