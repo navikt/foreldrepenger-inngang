c5a437120ee371e18d262aa9c97ce35741c7611e
Andreas Dybdahl <andreas.dybdahl@nav.no>
2020-06-25 16:42:59 +0200

    Intl stuff

--- src/app/bootstrap.tsx
+++ src/app/bootstrap.tsx
@@ -11,6 +11,11 @@ import './styles/index.less';
 
 const root = document.getElementById('app');
 
+if (!Intl.PluralRules) {
+    require('@formatjs/intl-pluralrules/polyfill');
+    require('@formatjs/intl-pluralrules/dist/locale-data/nb');
+}
+
 Sentry.init({
     dsn: 'https://94b48adfe3f54da89bba5e481e3c0f31@sentry.gc.nav.no/5',
     environment: window.location.hostname,
--- src/app/intl/IntlProvider.tsx
+++ src/app/intl/IntlProvider.tsx
@@ -1,29 +1,18 @@
 import * as React from 'react';
 import { IntlProvider as Provider } from 'react-intl';
+import moment from 'moment';
 
 import enMessages from './locales/en_GB.json';
 import nnMessages from './locales/nn_NO.json';
 import nbMessages from './locales/nb_NO.json';
 
-import areIntlLocalesSupported from 'intl-locales-supported';
-
 export type Language = 'nb' | 'nn' | 'en';
-
-const localesMyAppSupports = ['nb-NO'];
-
-if (global.Intl) {
-    if (!areIntlLocalesSupported(localesMyAppSupports)) {
-        const IntlPolyfill = require('intl');
-        Intl.NumberFormat = IntlPolyfill.NumberFormat;
-        Intl.DateTimeFormat = IntlPolyfill.DateTimeFormat;
-    }
-} else {
-    global.Intl = require('intl');
-}
 
 interface StateProps {
     language: Language;
 }
+
+moment.locale('nb');
 
 class IntlProvider extends React.Component<StateProps> {
     constructor(props: StateProps) {
