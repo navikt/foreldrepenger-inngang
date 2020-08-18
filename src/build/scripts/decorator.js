90e68d9184260bef73c3d95456c1037e4a44167e
Andreas Dybdahl <andreas.dybdahl@nav.no>
2020-08-18 11:18:29 +0200

    Legg med chatbot parameter i dekoratør kall

--- src/build/scripts/decorator.js
+++ src/build/scripts/decorator.js
@@ -5,7 +5,7 @@ const { JSDOM } = jsdom;
 
 const requestDecorator = (callback) => {
     const baseUrl = process.env.APPRES_CMS_URL;
-    return request(`${baseUrl}`, callback);
+    return request(`${baseUrl}/?chatbot=true`, callback);
 };
 
 const getDecorator = () =>
