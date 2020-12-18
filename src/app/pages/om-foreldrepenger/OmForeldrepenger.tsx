36e0cf3b567d9793aee2fbce5b594d318783f2ef
MagnusRom <magnus.skalleberg.rom@nav.no>
2020-12-18 12:31:59 +0100

    added hurtiglenke

--- src/app/pages/om-foreldrepenger/OmForeldrepenger.tsx
+++ src/app/pages/om-foreldrepenger/OmForeldrepenger.tsx
@@ -42,6 +42,7 @@ const sections: Section[] = [
     Section.HvisEnAvDereBlirSyke,
     Section.HjemmeSamtidig,
     Section.Adoptere,
+    Section.MorHarUføretrygd,
     Section.EtterDuHarSøkt,
     Section.OfteStilteSpørsmål,
 ];
