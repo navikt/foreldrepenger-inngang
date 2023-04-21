02a9884005fe1ac2ae2d77f32bbb3e1339b96cee
tor-nav <tor.roneid@nav.no>
2023-04-21 09:51:26 +0200

    Dropp fiksing av test-oppsett

--- jest/jest.config.js
+++ jest/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    setupFilesAfterEnv: ['./jest/setup.ts'],
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+    testRegex: '(/__tests/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
     moduleNameMapper: {
         '\\.(css|jpg|png|svg|less)$': '<rootDir>/jest/style-mock.js',
         'nav-(.*)-style': '<rootDir>/jest/style-mock.js',
@@ -8,13 +7,7 @@ module.exports = {
         '^common/(.*)': '<rootDir>/src/common/$1',
         '^app/(.*)': '<rootDir>/src/app/$1',
     },
-    globals: {
-        'ts-jest': {
-            tsConfig: './tsconfig.test.json',
-            babelConfig: true,
-        },
-    },
     rootDir: '../',
-    preset: 'ts-jest/presets/js-with-babel',
+    preset: 'ts-jest/presets/js-with-ts',
     testMatch: null,
 };
