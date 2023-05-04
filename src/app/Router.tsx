246b3571539fc57440d6cd60d44eda7cd092bb86
Tor Røneid <tor.roneid@nav.no>
2023-05-03 23:53:28 -1200

    Oppgradert til node 18 + deps + ta vekk rute om-engangsstonad (#1464)

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -6,7 +6,6 @@ import Dokumentasjon from './pages/dokumentasjon/Dokumentasjon';
 import HvaSøkerDu from './pages/hva-søker-du/HvaSøkerDu';
 import Informasjonstavle from './pages/informasjonstavle/Informasjonstavle';
 import Kalkulator from './pages/kalkulator/Kalkulator';
-import OmEngangsstønad from './pages/om-engangsstønad/OmEngangsstønad';
 import OmSvangerskapspenger from './pages/svangerskapspenger/OmSvangerskapspenger';
 import Regelendringer from './pages/regelendringer/Regelendringer';
 import SøkForeldrepenger from './pages/søk-foreldrepenger/SøkForeldrepenger';
@@ -18,7 +17,6 @@ const Router = () => (
         <Route path={Page.Dokumentasjon} element={<Dokumentasjon />} />
         <Route path={Page.HvaSøkerDu} element={<HvaSøkerDu />} />
         <Route path={Page.HvorMye} element={<Kalkulator />} />
-        <Route path={Page.OmEngangsstønad} element={<OmEngangsstønad />} />
         <Route path={Page.SøkeOmForeldrepenger} element={<SøkForeldrepenger />} />
         <Route path={Page.Regelendringer} element={<Regelendringer />} />
         <Route path={Page.OmSvangerskapspenger} element={<OmSvangerskapspenger />} />
