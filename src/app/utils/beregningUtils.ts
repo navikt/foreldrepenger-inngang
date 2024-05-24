cd635176f06f81fc91222313d5943e261170a4f3
Andreas Dybdahl <andreas.dyb@gmail.com>
2024-05-24 13:29:13 +0200

    Oppdatert grunnbeløp

--- src/app/utils/beregningUtils.ts
+++ src/app/utils/beregningUtils.ts
@@ -1,7 +1,7 @@
 import moment from 'moment';
 
 const AVVIKSGRENSE = 0.25;
-export const GRUNNBELØPET = 118620;
+export const GRUNNBELØPET = 124028;
 export const ENGANGSSUM_PER_BARN = 90300;
 export const MÅNEDSLØNN_EKSEMPEL = 25000;
 export const ÅRSLØNN_EKSEMPEL = 250000;
