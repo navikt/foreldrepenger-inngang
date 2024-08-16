a804269445376e86d111cd165b329a87ebdf4c8f
tor-nav <tor.roneid@nav.no>
2024-08-16 10:48:26 +0200

    Redirect til ny vegvisar for hvor-mye

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,22 +1,33 @@
 import * as React from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
-import { Page } from './types/Page';
 import Dokumentasjon from './pages/dokumentasjon/Dokumentasjon';
 import HvaSøkerDu from './pages/hva-søker-du/HvaSøkerDu';
 import Informasjonstavle from './pages/informasjonstavle/Informasjonstavle';
-import Kalkulator from './pages/kalkulator/Kalkulator';
-import OmSvangerskapspenger from './pages/svangerskapspenger/OmSvangerskapspenger';
 import Regelendringer from './pages/regelendringer/Regelendringer';
+import OmSvangerskapspenger from './pages/svangerskapspenger/OmSvangerskapspenger';
 import SøkForeldrepenger from './pages/søk-foreldrepenger/SøkForeldrepenger';
 import Veiviser from './pages/veiviser/Veiviser';
+import { Page } from './types/Page';
+
+const RedirectUrl: React.FunctionComponent<{ url: string }> = ({ url }) => {
+    React.useEffect(() => {
+        window.location.href = url;
+    }, [url]);
+
+    return <h5>Redirecting...</h5>;
+};
 
 const Router = () => (
     <Routes>
         <Route path={Page.Root} element={<Informasjonstavle />} />
         <Route path={Page.Dokumentasjon} element={<Dokumentasjon />} />
         <Route path={Page.HvaSøkerDu} element={<HvaSøkerDu />} />
-        <Route path={Page.HvorMye} element={<Kalkulator />} />
+        <Route path={Page.HvorMye} element={<Navigate to={Page.Root} key="redirect" />} />
+        <Route
+            path={Page.HvorMye}
+            element={<RedirectUrl url="https://www.nav.no/foreldrepenger/veivisere/hvor-mye" />}
+        />
         <Route path={Page.SøkeOmForeldrepenger} element={<SøkForeldrepenger />} />
         <Route path={Page.Regelendringer} element={<Regelendringer />} />
         <Route path={Page.OmSvangerskapspenger} element={<OmSvangerskapspenger />} />
